5e184ca28b96f65810d1471c4b632ed5e40df411
BINGCHUN CHANG <122491347+CharlieCBC@users.noreply.github.com>
2024-04-12 19:50:52 -0400

    Till 3.5.1 finished

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
 import express from "express";
+import mongoose from "mongoose";
+import UserRoutes from "./Users/routes.js";
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5.js";
 import CourseRoutes from "./Kanbas/courses/routes.js";
@@ -6,9 +8,11 @@ import ModuleRoutes from "./Kanbas/modules/routes.js";
 import AssignmentRoutes from "./Kanbas/assignments/routes.js";
 import cors from "cors";
 
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 const app = express();
 app.use(cors());
 app.use(express.json());
+UserRoutes(app);
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
