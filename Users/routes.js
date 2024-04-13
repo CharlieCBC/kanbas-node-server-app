f6bb88ba02bc6ae6e17003827d26cfbf8c5a1dcd
BINGCHUN CHANG <122491347+CharlieCBC@users.noreply.github.com>
2024-04-12 21:26:00 -0400

    3.5.8 done

--- Users/routes.js
+++ Users/routes.js
@@ -12,6 +12,13 @@ export default function UserRoutes(app) {
   };
 
   const findAllUsers = async (req, res) => {
+    const { role } = req.query;
+    if (role) {
+      const users = await dao.findUsersByRole(role);
+      res.json(users);
+      return;
+    }
+
     const users = await dao.findAllUsers();
     res.json(users);
   };
