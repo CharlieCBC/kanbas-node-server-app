4784cd0ba1adec71a8e4e6e79dfeaa15e973992d
BINGCHUN CHANG <122491347+CharlieCBC@users.noreply.github.com>
2024-04-14 08:48:52 -0400

    update error message

--- Users/routes.js
+++ Users/routes.js
@@ -50,11 +50,11 @@ export default function UserRoutes(app) {
 
   const signin = async (req, res) => {
     const { username, password } = req.body;
-    const currentUser = await dao.findUserByCredentials(username, password);
     if (!username || !password) {
       res.sendStatus(400).json({ message: "Missing username or password" });
       return;
     }
+    const currentUser = await dao.findUserByCredentials(username, password);
     if (currentUser) {
       req.session["currentUser"] = currentUser;
       globalCurrentuser = currentUser;
